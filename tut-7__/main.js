7b79a95de486652e6f1f4cd6b4b8dff32bc05f9a
Md Shayon <mdshayon0@gmail.com>
2019-01-11 22:16:29 +0600

    Start and stop animation

--- tut-7__/main.js
+++ tut-7__/main.js
@@ -1,12 +1,30 @@
-$(document).ready(function(){
-    $(".content-box").click(function(){
+$(document).ready(function () {
+    $(".content-box").click(function () {
         $(this).animate({
             width: '500px',
             // DEFAULT VALUE PLUS EXTRA VALUE SETTING
-            height: '+=100px'
-        }, 1000);
+            //height: '+=100px'
+            height: '400px'
+        }, 3000).animate({
+            fontSize: '30px' 
+        }, 3000).fadeOut();
+
+
+        $("button").click(function(){
+            $('.content-box').stop(true, true);
+        });
+
     });
+
+
+
+
+
+    //ANIMATING OPACITY
+    /*
     $('.content-after').show().animate({
         opacity: '1',
-    },3000);
+    },3000);*/
+
+
 });
