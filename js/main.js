538e7367c89b2e2faa47cac346524075ad36e634
Md Shayon <mdshayon0@gmail.com>
2019-01-11 17:28:44 +0600

    Initial code.how to add jquery to website, syntex defference and many more

---
+++ js/main.js
@@ -0,0 +1,23 @@
+/*document.getElementById("test").onclick = function (){
+    alert('Clicked');
+}*/
+
+
+/*
+// SAME THING WITH JQUERY. IF WE WRITE JS FILE LINK INSIDE HEAD TAG IN HTML THIS CODE WILL WORK OTHERWISE NOT
+$(document).ready(function () {
+    $("#test").click(function () {
+        alert('clicked');
+    });
+});*/
+
+
+
+
+
+// SAME THING WITH JQUERY
+/*
+$("#test").click(function () {
+    $(this).hide();
+});*/
+
